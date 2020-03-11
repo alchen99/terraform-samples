49f2ef547b1ba3b6792f47825bd4fcbf76d5a55a
Alice Chen <alice.chen@armory.io>
2020-03-11 10:19:32 -0700

    Fix remote state

--- aws/01-s3/remote-state.tf
+++ aws/01-s3/remote-state.tf
@@ -1,5 +1,6 @@
 data "terraform_remote_state" "random_string" {
   backend = "s3"
+  workspace = terraform.workspace
   config = {
     region         = "us-east-1"
     bucket         = "alc-tf-samples-state"
