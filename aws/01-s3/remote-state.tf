90db1a554603133a020e42daf77c2a4b6713c98a
Alice Chen <alice.chen@armory.io>
2022-01-31 05:03:00 -0800

    Rename tfstate

--- aws/01-s3/remote-state.tf
+++ aws/01-s3/remote-state.tf
@@ -4,7 +4,7 @@ data "terraform_remote_state" "random_string" {
   config = {
     region         = "us-east-1"
     bucket         = "alc-tf-samples-state"
-    key            = "public-samples/random.tfstate"
+    key            = "public-samples/random00.tfstate"
     dynamodb_table = "alc-tf-samples-state"
     encrypt        = "true"
   }
