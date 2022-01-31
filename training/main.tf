c57afc2a9b9632eeeef6a816f2d7b6a5f1ccb0fe
Alice Chen <alice.chen@armory.io>
2022-01-31 05:46:15 -0800

    Add training example

---
+++ training/main.tf
@@ -0,0 +1,59 @@
+# generate a random string with no special characters and use as suffix
+resource "random_string" "suffix" {
+  length  = 10
+  upper   = false
+  special = false
+}
+
+output "suffix" {
+  value = random_string.suffix.id
+}
+
+locals {
+  s3_bucket_name_local = "%{ if var.add_suffix }${local.s3_bucket_name}-${random_string.suffix.id}%{ else }${local.s3_bucket_name}%{ endif }"
+}
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  version = "1.25.0"
+
+  create_bucket = var.create_s3_bucket
+
+  bucket = local.s3_bucket_name_local
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+  server_side_encryption_configuration = {
+    rule  = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    env   = var.env
+    owner = var.owner
+  }
+}
+
+data "aws_s3_bucket" "selected" {
+  depends_on = [ module.s3_bucket ]
+
+  bucket = local.s3_bucket_name_local
+}
+
+output "s3_bucket_id" {
+  value = data.aws_s3_bucket.selected.id
+}
+
+output "s3_bucket_arn" {
+  value = data.aws_s3_bucket.selected.arn
+}
+
+output "s3_bucket_domain_name" {
+  value = data.aws_s3_bucket.selected.bucket_domain_name
+}
